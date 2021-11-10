f8bc95cafe0c14fa840c67b344404100024297f9
Natan <natanwsn@gmail.com>
2021-11-10 10:39:44 -0300

    fix: fixed some issues with global styles not working because of quotation marks between the rgba value

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import React from "react";
-import GlobalStyles from "./styles/global"
-
+import GlobalStyles from "./styles/global";
 
 function App() {
   return (
-  <>
-  <div>App</div>
-  <GlobalStyles/>
-  </>
+    <>
+      <GlobalStyles />
+      <div>App</div>
+    </>
   );
 }
 
