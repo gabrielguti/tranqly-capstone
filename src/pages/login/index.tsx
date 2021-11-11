0758460a8cb3ec97726f3203835a218ddbed4935
Kaueh <kaueh1234@gmail.com>
2021-11-10 23:01:03 -0300

    feat: Login Page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,27 @@
+import { MainContainer, FormContainer } from "./styles";
+import Header from "../../components/header";
+import LoginImg from "../../assets/img/IllustrationR18.svg";
+
 const Login = () => {
-  return <></>;
+  return (
+    <MainContainer>
+      <Header />
+      <FormContainer>
+        <img src={LoginImg} alt={"LoginImg"} />
+        <h1>Entrar</h1>
+        <div className="FormDiv">
+          <form>
+            <input placeholder="Email" />
+            <input placeholder="Senha" />
+            <button>Entrar</button>
+          </form>
+          <p>
+            Não possui conta? <a href="/singup">Cadastre-se</a>
+          </p>
+        </div>
+      </FormContainer>
+    </MainContainer>
+  );
 };
 
 export default Login;
